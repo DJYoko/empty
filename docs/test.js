52500674d78e3cd18f29c87deaaccf6bcc85bf43
dj_yoko <yokochan.recurrent@gmail.com>
2019-02-12 00:22:46 +0900

    add test case for string "".

--- docs/test.js
+++ docs/test.js
@@ -1,10 +1,9 @@
-(() => {
-	const _assert = chai.assert;
+const _assert = chai.assert;
 
-	describe('test for "empty"', function () {
-		it('', function () {
-			_assert.strictEqual(empty(''), true);
-		});
 
+describe('test for "empty"', function () {
+	it('', function () {
+		_assert.strictEqual(empty(''), true);
 	});
-})
+
+});
