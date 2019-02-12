9dae158ee2a566de73077ca69b3049d6cb04b73e
dj_yoko <yokochan.recurrent@gmail.com>
2019-02-12 23:08:01 +0900

    add test case for num & object & boolean.

--- docs/test.js
+++ docs/test.js
@@ -1,9 +1,21 @@
 const _assert = chai.assert;
 
+const TRUE_CASES = [
+    "",
+    0,
+    0.0,
+    "0",
+    null,
+    false,
+    [],
+];
 
-describe('test for "empty"', function () {
-	it('', function () {
-		_assert.strictEqual(empty(''), true);
-	});
-
+describe('test for empty.js', () => {
+    TRUE_CASES.forEach((item) => {
+        const type = typeof(item);
+        const message = 'type: ' + type + ' / "' + item + '" returns true ';
+        it(message, () => {
+            _assert.strictEqual(empty(''), true);
+        });
+    });
 });
